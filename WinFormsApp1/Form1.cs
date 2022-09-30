cbbc8dacfcf656ead4f0689d36f1c679f970febf
Prince Mishra <Prince.m@curiticshealth.com>
2022-09-30 13:09:44 +0530

    new repo

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static string smtpAddress = "smtp.gmail.com";
+        static int portNumber = 587;
+        static bool enableSSL = true;
+        static string emailFromAddress = "tester55527@gmail.com"; //Sender Email Address  
+        static string password = "tester12345@"; //Sender Password  
+        static string emailToAddress = "prince.chealth@gmail.com"; //Receiver Email Address  
+        static string subject = "Hello";
+        static string body = "Hello, This is Email sending test using gmail.";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public static void SendEmail()
+        {
+            /*using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress(emailFromAddress);
+                mail.To.Add(emailToAddress);
+                mail.Subject = subject;
+                mail.Body = body;
+                mail.IsBodyHtml = true;
+                //mail.Attachments.Add(new Attachment("D:\\TestFile.txt"));//--Uncomment this to send any attachment  
+                using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+                {
+                    smtp.Credentials = new NetworkCredential(emailFromAddress, password);
+                    smtp.EnableSsl = enableSSL;
+                    smtp.Send(mail);
+                }
+
+            }
+        }*/
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+                var fromAddress = "golu21397@outlook.com";
+                var toAddress = "prince.chealth@gmail.com";
+            const string fromPassword = "Vishal21397@";
+                string body = "Welcome..";
+                // smtp settings
+                var smtp = new System.Net.Mail.SmtpClient();
+                {
+                    smtp.Host = "smtp.office365.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+
+                    smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+
+                smtp.Credentials = new NetworkCredential("golu21397@outlook.com", "Vishal21397@");
+                    smtp.Timeout = 600000;
+                }
+                // Passing values to smtp object
+                smtp.Send(fromAddress, toAddress, subject, body);
+            }
+        }
+    }
+
+
+
+    /*SendEmail*/
+  /*  ();*/
+      
